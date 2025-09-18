5de20afcc4cc4fa87af8930c30d696af7149ee15
arun-prakash-official <arunprakash202005@gmail.com>
2025-09-18 12:15:59 +0530

    Initial commit

---
+++ VirusGrowth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class VirusGrowth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int days = sc.nextInt();
+        sc.close();
+        float inc_val=0.5f;
+        for(int i=1;i<=days;i++){
+            System.out.printf("%.1f ", inc_val);
+            inc_val *= 3;
+        }
+    }
+}
