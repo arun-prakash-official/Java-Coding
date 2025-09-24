380d3c9bc08a5d0950058c7e084caf703a8a91d7
arun-prakash-official <arunprakash202005@gmail.com>
2025-09-24 15:11:17 +0530

    Initiall commit

---
+++ Arrays/Matrix90Rotation.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Matrix90Rotation {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(rotated[i][j] + " ");
+            }
+            System.out.println();
+        }   
+    }
+}
