80a0dc1cc4a6d787f91b970bf21688fffd66bf7f
arun-prakash-official <arunprakash202005@gmail.com>
2026-08-10 14:06:26 +0530

    added file

--- Fibbonacci.java
+++ Fibbonacci.java
@@ -4,13 +4,16 @@ public class Fibbonacci {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a = 0, b = 1;
+        int total=0;
         sc.close();
         System.out.print(a + " " + b + " ");
         for (int i = 2; i < n; i++) {
             int c = a + b;
             System.out.print(c + " ");
+            total+=c;
             a = b;
             b = c;
         }
+        System.out.println("\nTotal: " + total);
     }
 }
