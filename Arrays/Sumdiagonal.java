5c2cdff1c39f1a2103d1f34db1e65334e0d95a76
arun-prakash-official <arunprakash202005@gmail.com>
2026-08-13 13:58:34 +0530

    added files

---
+++ Arrays/Sumdiagonal.java
@@ -0,0 +1,17 @@
+package Arrays;
+public class Sumdiagonal {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i]; // Summing the diagonal elements
+        }
+
+        System.out.println("Sum of diagonal elements: " + sum);
+    }
+}
